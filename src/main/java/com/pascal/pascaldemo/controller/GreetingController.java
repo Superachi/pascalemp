08ea03bb6d778909d661bcb27ba3c2192ed15ef1
PascalKoeiman <psclkmn@gmail.com>
2022-03-02 22:08:03 +0100

    Update controller action to accept post requests

--- src/main/java/com/pascal/pascaldemo/controller/GreetingController.java
+++ src/main/java/com/pascal/pascaldemo/controller/GreetingController.java
@@ -1,8 +1,6 @@
 package com.pascal.pascaldemo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,9 +8,13 @@ import org.apache.logging.log4j.Logger;
 public class GreetingController {
     private static final Logger logger = LogManager.getLogger("HelloWorld");
 
-    @RequestMapping(path = "/greeting", method = RequestMethod.GET)
-    public String getGreeting() {
-        logger.info("The logger is totally working!!");
-        return "Hello I'm basic";
+    // Using Postman, user can post a string, which could contain a variable interpolation
+    // which Log4J will try to resolve
+    // https://commons.apache.org/proper/commons-configuration//userguide/howto_basicfeatures.html#Variable_Interpolation
+    // Try this input for the POST-request https://log4shell.tools/
+    @RequestMapping(method = RequestMethod.POST, value="/bad_endpoint")
+    public String getGreeting(@RequestBody String query) {
+        logger.warn("The definitely trustworthy output is: {}", query);
+        return "Success";
     }
 }
