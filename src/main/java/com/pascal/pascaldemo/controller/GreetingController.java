6717db6f61829c1ef08bb45c054b6b64a7c0155f
PascalKoeiman <psclkmn@gmail.com>
2022-03-02 09:13:13 +0100

    Add first logging instance

--- src/main/java/com/pascal/pascaldemo/controller/GreetingController.java
+++ src/main/java/com/pascal/pascaldemo/controller/GreetingController.java
@@ -3,11 +3,16 @@ package com.pascal.pascaldemo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @RestController
 public class GreetingController {
+    private static final Logger logger = LogManager.getLogger("HelloWorld");
+
     @RequestMapping(path = "/greeting", method = RequestMethod.GET)
     public String getGreeting() {
+        logger.info("The logger is totally working!!");
         return "Hello I'm basic";
     }
 }
